faf094d0a357076613908270aca0335a9e27b31d
Julian <parrishjulian62@gmail.com>
2017-10-17 19:39:59 -0400

    Add /api to end of API URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 require 'api_constraints'
 ApiKarmaS::Application.routes.draw do
-  namespace :api, defaults: {format: :json},
-                            constraints: {subdomain: 'api'}, path: '/' do
-    scope module: :v1,
-      constraints: ApiConstraints.new(version: 1, default: true) do
-          resources   :users,    :only => [:show,   :create, :update, :destroy] do
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+          resources   :users,    only: [:show,   :create, :update, :destroy] do
             resources   :stylists, :only => [:create, :update, :destroy] do
               resources :image_portfolios, :only => [:show, :create, :index]
               resources :appointments, :only => [:show, :create]
